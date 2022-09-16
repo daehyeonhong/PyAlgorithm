7aedbc175232994b03efb381e01ff1a4dc00ca50
DaeHyeonHong <fholng@outlook.com>
2022-09-16 17:55:38 +0900

    최댓값 위치

--- ch1/p02-2-findmaxindex.py
+++ ch1/p02-2-findmaxindex.py
@@ -2,11 +2,9 @@
 # output: 최댓값 위치
 
 def find_max(a):
-    max_v: Int = a[0]
     max_v_ind = 0
     for i in range(len(a)):
-        max_v_ind = i if a[i] > max_v else max_v_ind
-        max_v = a[i] if a[i] > max_v else max_v
+        max_v_ind = i if a[i] > a[max_v_ind] else max_v_ind
     return max_v_ind
 
 
