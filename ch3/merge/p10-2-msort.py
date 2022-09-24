582a0882ba3ca8c74dc61eaa44369d6959c2aea1
DaeHyeonHong <fholng@outlook.com>
2022-09-24 16:28:23 +0900

    병합정렬

---
+++ ch3/merge/p10-2-msort.py
@@ -0,0 +1,39 @@
+def merge_sort(arg, n, st):
+    n += 1
+    print(f'st : {st}, idx : {n}, arg : {arg}')
+    length = len(arg)
+    if length <= 1:
+        return
+
+    center = length // 2
+    group1 = arg[:center]
+    group2 = arg[center:]
+    merge_sort(group1, n, 'g1')
+    merge_sort(group2, n, 'g2')
+    print(f'st : {st}, idx : {n}, arg : {arg}, group1 : {group1}, group2 : {group2}')
+    i1 = 0
+    i2 = 0
+    ia = 0
+
+    while i1 < len(group1) and i2 < len(group2):
+        if group1[i1] < group2[i2]:
+            arg[ia] = group1[i1]
+            i1 += 1
+            ia += 1
+        else:
+            arg[ia] = group2[i2]
+            ia += 1
+            i2 += 1
+    while i1 < len(group1):
+        arg[ia] = group1[i1]
+        i1 += 1
+        ia += 1
+    while i2 < len(group2):
+        arg[ia] = group2[i2]
+        ia += 1
+        i2 += 1
+
+
+arr = [6, 8, 3, 9, 10, 1, 2, 4, 7, 5]
+merge_sort(arr, -1, 'root')
+print(f'result : {arr}')
