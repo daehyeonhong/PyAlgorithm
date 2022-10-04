c2440ae8d7b4130392a834a3f759dad1df198a60
HonGDae <fholng@outlook.com>
2022-10-04 16:31:47 +0900

    버블정렬 연습문제

---
+++ ch3/quick/11-1-bubblesort.py
@@ -0,0 +1,28 @@
+def bubble_sort(arg):
+    length = len(arg)
+    cnt = 1
+    while cnt != 0:
+        cnt = 0
+        for i in range(0, length - 1):
+            j = i + 1
+            i_val = arg[i]
+            j_val = arg[j]
+            if i_val > j_val:
+                print(f'before : {arg}')
+                print(f'change arg[i] : {i_val}, arg[j] : {j_val}')
+                arg[i], arg[j] = arg[j], arg[i]
+                cnt += 1
+                print(f'after : {arg}')
+            else:
+                print(f'not change arg[i] : {i_val}, arg[j] : {j_val}')
+
+
+arr = [2, 4, 5, 1, 3]
+arr2 = [3, 4, 5, 2, 1]
+arr3 = [5, 4, 3, 2, 1]
+bubble_sort(arr)
+print(arr)
+bubble_sort(arr2)
+print(arr2)
+bubble_sort(arr3)
+print(arr3)
