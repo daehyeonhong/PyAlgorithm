8b93e085727d2e0890d47cf910c66e526dcd7673
DaeHyeonHong <fholng@outlook.com>
2022-10-15 22:24:22 +0900

    산술평균_1

--- baekjoon/sort/2108.py
+++ baekjoon/sort/2108.py
@@ -5,6 +5,9 @@ for i in range(input_val):
 result_val_arr = sorted(input_val_arr)
 for r_idx in range(len(result_val_arr)):
     print(result_val_arr[r_idx])
+print(산술평균(input_val))
+print(중앙값(input_val))
+print(범위(input_val))
 
 
 def 산술평균(arr):
@@ -13,3 +16,11 @@ def 산술평균(arr):
     for i in range(length):
         sum += arr[i]
     return round(sum / length)
+
+
+def 중앙값(arr):
+    return arr[len(arr) / 2]
+
+
+def 범위(arr):
+    return arr[len(arr)] - arr[0]
