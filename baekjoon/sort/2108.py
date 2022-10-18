f7c611ccb2d302ddd937da55a9594f0335105417
HongDaeHyeon <fholng@outlook.com>
2022-10-18 21:05:43 +0900

    25305 2108

--- baekjoon/sort/2108.py
+++ baekjoon/sort/2108.py
@@ -1,49 +1,35 @@
-from collections import Counter
-import math
 import sys
+from collections import Counter
 
 
-def 산술평균(arr):
-    length = len(arr)
-    sum = 0
-    for i in range(length):
-        sum += arr[i]
-    return round(sum / length)
+def 평균(arg, leng):
+    return round(sum(arg) / leng)
 
 
-def 중앙값(arr):
-    return arr[math.floor(len(arr) / 2)]
+def 중앙값(arg, leng):
+    return arg[leng // 2]
 
 
-def 범위(arr):
-    return arr[len(arr)] - arr[0]
-
-
-def 최빈값(arr, n):
-    n = int(sys.stdin.readline())
-    nums = []
-    for i in range(n):
-        nums.append(int(sys.stdin.readline()))
-    nums.sort()
-    nums_s = Counter(nums).most_common()
-    print(round(sum(nums) / n))
-    print(nums[n // 2])
-    if len(nums_s) > 1:
-        if nums_s[0][1] == nums_s[1][1]:
-            print(nums_s[1][0])
-        else:
-            print(nums_s[0][0])
+def 최빈값(arg):
+    args = Counter(arg).most_common()
+    print(args)
+    print(Counter(arg))
+    if len(args) > 1:
+        return args[1][0] if args[0][1] == args[1][1] else args[0][0]
     else:
-        print(nums_s[0][0])
-    print(nums[-1] - nums[0])
+        return args[0][0]
 
 
-input_val = int(input())
-input_val_arr = []
-for i in range(input_val):
-    input_val_arr.append(int(input()))
-result_val_arr = sorted(input_val_arr)
-print(산술평균(result_val_arr))
-print(중앙값(result_val_arr))
-print(최빈값(result_val_arr, input_val))
-print(범위(result_val_arr))
+def 범위(arg):
+    return arg[-1] - arg[0]
+
+
+length = int(sys.stdin.readline())
+array = []
+for i in range(length):
+    array.append(int(sys.stdin.readline()))
+array.sort()
+print(f'평균{평균(array, length)}')
+print(f'중앙값{중앙값(array, length)}')
+print(f'최빈값{최빈값(array)}')
+print(f'범위{범위(array)}')
