da7bcc26a4c6d00e8435e4a2729d8460103559dc
HongDaeHyeon <fholng@outlook.com>
2022-10-24 22:29:24 +0900

    1181_개선

--- baekjoon/sort/1181.py
+++ baekjoon/sort/1181.py
@@ -1,26 +1,11 @@
 import sys
 
 length = int(sys.stdin.readline())
-funcSet = [{}] * 51
+arr = []
 for i in range(length):
-    inp = sys.stdin.readline().rstrip()
-    len1 = len(inp)
-    len_ = funcSet[len1]
-    if len_ == {}:
-        funcSet[len1] = {inp}
-    else:
-        len_.add(inp)
-        funcSet[len1] = len_
-for a in funcSet:
-    a_len = len(a)
-    if a == {}:
-        continue
-    elif a_len == 1:
-        print(a.pop())
-    else:
-        semi_set = []
-        while a:
-            semi_set.append(a.pop())
-        semi_set.sort(reverse=True)
-        while semi_set:
-            print(semi_set.pop())
+    arr.append(sys.stdin.readline().rstrip())
+l = list(set(arr))
+l.sort()
+l.sort(key=len)
+for i in l:
+    print(i)
