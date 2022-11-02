316856874f33bea40a46d475387a67ebf7b81073
DaeHyeonHong <fholng@outlook.com>
2022-11-03 01:39:20 +0900

    1914 하노이의 탑

--- baekjoon/6/1914.py
+++ baekjoon/6/1914.py
@@ -1,29 +1,16 @@
 import sys
 
 
-def det(num):
-    for v in range(num):
-        if arr[num] == arr[v] or abs(arr[num] - arr[v]) == num - v:
-            return False
-    return True
-
-
-def func(num):
-    global cnt
-
-    if num == N:
-        cnt += 1
-        return
-
-    for i in range(N):
-        arr[num] = i
-        if det(num):
-            func(num + 1)
+def hanoi(val, fst, mid, lst):
+    if val == 1:
+        print(fst, lst, sep=" ")
+    else:
+        hanoi(val - 1, fst, lst, mid)
+        hanoi(1, fst, mid, lst)
+        hanoi(val - 1, mid, fst, lst)
 
 
 N = int(sys.stdin.readline())
-arr = [0] * N
-cnt = 0
-
-func(0)
-print(cnt)
+print(2 ** N - 1)
+if N <= 20:
+    hanoi(N, 1, 2, 3)
