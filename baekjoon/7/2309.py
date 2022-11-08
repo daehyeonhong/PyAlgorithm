0f38540d6c2987d151f912e49eb05ae3908ee677
HongDaeHyeon <fholng@outlook.com>
2022-11-08 21:31:08 +0900

    리팩토링

--- baekjoon/7/2309.py
+++ baekjoon/7/2309.py
@@ -1,28 +1,10 @@
-import sys
+import itertools
 
-arr = []
-for i in range(9):
-    arr.append(int(sys.stdin.readline()))
+array = [int(input()) for _ in range(9)]
 
-arr.sort()
-sum_val = sum(arr)
-diff = sum_val - 100
-target = []
-
-
-def method_name(arg):
-    length = len(arg)
-    for idx in range(length):
-        for j in range(idx + 1, length):
-            i_ = arr[idx]
-            j_ = arr[j]
-            if i_ + j_ == diff:
-                arr.pop(arr.index(i_))
-                arr.pop(arr.index(j_))
-                return arr
-            elif i_ + j_ > diff:
-                continue
-
-
-for item in method_name(arr):
-    print(item)
+combinations = itertools.combinations(array, 7)
+for i in combinations:
+    if sum(i) == 100:
+        for j in sorted(i):
+            print(j)
+        break
