bd2233d829d20c3e53fc6eff86b3ef9dc682ce2f
DaeHyeonHong <fholng@outlook.com>
2022-11-09 23:45:28 +0900

    10819

--- baekjoon/7/10819.py
+++ baekjoon/7/10819.py
@@ -2,20 +2,18 @@ from itertools import permutations
 
 import sys
 
-# 주어진 값 입력
 n = int(sys.stdin.readline())
 a = list(map(int, sys.stdin.readline().split()))
 
-# permutation 저장(per: reference of permutation tuples)
-per = permutations(a)
 ans = 0
-
-# 순열마다 차이를 더해(s), ans 보다 크면 ans를 update
-for item in per:
+result_item = []
+for item in permutations(a):
     s = 0
     for job in range(len(item) - 1):
         s += abs(item[job] - item[job + 1])
     if s > ans:
+        result_item = item
         ans = s
 
+print(result_item)
 print(ans)
