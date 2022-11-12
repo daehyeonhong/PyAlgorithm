e9125632b5db36d1482cbccef60120c54f6b7f5e
HongDaeHyeon <fholng@outlook.com>
2022-11-12 22:24:33 +0900

    25501

---
+++ baekjoon/recursion/1/25501.py
@@ -0,0 +1,15 @@
+def recursion(s, l, r):
+    if l >= r:
+        return 1
+    elif s[l] != s[r]:
+        return 0
+    else:
+        return recursion(s, l + 1, r - 1)
+
+
+def is_palindrome(s):
+    return recursion(s, 0, len(s) - 1)
+
+
+print('ABBA:', is_palindrome('ABBA'))
+print('ABC:', is_palindrome('ABC'))
