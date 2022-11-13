82d99bd41b76778511246b9494bf557959693a13
HongDaeHyeon <fholng@outlook.com>
2022-11-13 17:17:11 +0900

    25501

--- baekjoon/recursion/1/25501.py
+++ baekjoon/recursion/1/25501.py
@@ -1,4 +1,9 @@
+import sys
+
+
 def recursion(s, l, r):
+    global cnt
+    cnt += 1
     if l >= r:
         return 1
     elif s[l] != s[r]:
@@ -7,9 +12,18 @@ def recursion(s, l, r):
         return recursion(s, l + 1, r - 1)
 
 
+cnt: int = 0
+
+
 def is_palindrome(s):
+    global cnt
+    cnt = 0
     return recursion(s, 0, len(s) - 1)
 
 
-print('ABBA:', is_palindrome('ABBA'))
-print('ABC:', is_palindrome('ABC'))
+readline = sys.stdin.readline
+T: int = int(readline())
+
+S = [readline().rstrip() for i in range(T)]
+for s in S:
+    print(is_palindrome(s), cnt)
