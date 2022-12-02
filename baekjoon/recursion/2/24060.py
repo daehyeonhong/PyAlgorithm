56d09f3d582432061513896f9463ef07d34e4c75
DaeHyeonHong <fholng@outlook.com>
2022-12-02 17:51:55 +0900

    24060

---
+++ baekjoon/recursion/2/24060.py
@@ -0,0 +1,54 @@
+import sys
+
+cnt: int = 0
+K, N = map(int, sys.stdin.readline().split())
+
+
+def merge_sort(arg):
+    global cnt
+    length = len(arg)
+    if length <= 1:
+        return
+
+    center = length // 2
+    group1 = arg[:center]
+    group2 = arg[center:]
+    merge_sort(group1)
+    merge_sort(group2)
+    i1 = 0
+    i2 = 0
+    ia = 0
+    while i1 < len(group1) and i2 < len(group2):
+        if group1[i1] < group2[i2]:
+            cnt += 1
+            if cnt == N:
+                print(group1[i1])
+            elif cnt > N:
+                print(-1)
+                break
+            arg[ia] = group1[i1]
+            i1 += 1
+            ia += 1
+        else:
+            cnt += 1
+            if cnt == N:
+                print(group1[i2])
+            elif cnt > N:
+                print(-1)
+                break
+            arg[ia] = group2[i2]
+            ia += 1
+            i2 += 1
+        sort_left_args(arg, group1, i1, ia)
+        sort_left_args(arg, group2, i2, ia)
+
+
+def sort_left_args(arg, group, idx, base):
+    while idx < len(group):
+        arg[base] = group[idx]
+        idx += 1
+        base += 1
+
+
+arr = list(map(int, sys.stdin.readline().split()))
+merge_sort(arr)
