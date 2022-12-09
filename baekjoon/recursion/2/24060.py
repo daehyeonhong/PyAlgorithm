5b069a82fef1608d2ce2223e34f4fce0788648a0
DaeHyeonHong <fholng@outlook.com>
2022-12-09 15:10:34 +0900

    recursion:24060

--- baekjoon/recursion/2/24060.py
+++ baekjoon/recursion/2/24060.py
@@ -1,16 +1,13 @@
 import sys
 
-cnt: int = 0
 K, N = map(int, sys.stdin.readline().split())
-result: int = 0
+result_array = []
 
 
 def merge_sort(arg):
     length = len(arg)
-    if length <= 1:
+    if length == 1:
         return
-    global cnt
-    global result
     center = length // 2
     group1 = arg[:center]
     group2 = arg[center:]
@@ -21,12 +18,10 @@ def merge_sort(arg):
     ia = 0
     while i1 < len(group1) and i2 < len(group2):
         if group1[i1] < group2[i2]:
-            cnt += 1
             arg[ia] = group1[i1]
             i1 += 1
             ia += 1
         else:
-            cnt += 1
             arg[ia] = group2[i2]
             ia += 1
             i2 += 1
@@ -36,11 +31,11 @@ def merge_sort(arg):
 
 def sort_left_args(arg, group, idx, base):
     while idx < len(group):
+        result_array.append(group[idx])
         arg[base] = group[idx]
         idx += 1
         base += 1
 
 
-arr = [11, 8, 3, 9, 10, 1, 2, 4, 7, 5]
-merge_sort(arr)
-print(result)
+merge_sort(list(map(int, sys.stdin.readline().split())))
+print(-1 if len(result_array) <= N else result_array[N])
