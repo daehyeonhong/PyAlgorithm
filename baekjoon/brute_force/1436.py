2ee58d4ecbf426b9753bd2156cf8e0b968357916
HongDaeHyeon <fholng@outlook.com>
2023-01-17 20:28:38 +0900

    영호감독숌 변경

--- baekjoon/brute_force/1436.py
+++ baekjoon/brute_force/1436.py
@@ -2,14 +2,11 @@ import sys
 
 N: int = int(sys.stdin.readline())
 base: int = 666
-interval: int = 10
-start: int = base
+key: int = base
 while True:
-    end: int = start * interval
-    for i in range(start, end):
-        if str(i).find(str(base)) != -1:
-            N -= 1
-            if N == 0:
-                print(i)
-                exit()
-    start = end
+    if str(key).find(str(base)) != -1:
+        N -= 1
+        if N == 0:
+            print(key)
+            exit()
+    key += 1
