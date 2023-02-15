85ac292634f445137f4d7ec3b43ffd436b1a5b36
HongDaeHyeon <fholng@outlook.com>
2023-02-15 20:42:48 +0900

    1269_1

--- baekjoon/set/1269.py
+++ baekjoon/set/1269.py
@@ -1,14 +1,6 @@
-import sys
+a, b = map(int, input().split())
 
-A, B = map(int, sys.stdin.readline().split())
+A = list(input().split())
+B = list(input().split())
 
-SET_A = set(map(int, sys.stdin.readline().rstrip().split()))
-SET_B = set(map(int, sys.stdin.readline().rstrip().split()))
-RESULT_B=list()
-for B in SET_B:
-    if SET_A.__contains__(B):
-        SET_A.remove(B)
-    else:
-        RESULT_B.append(B)
-
-print(len(SET_A)+len(RESULT_B))
+print(2 * len(set(A + B)) - a - b)
