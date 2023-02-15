27d097e11a219a999e1a11b1611f0890d6314369
HongDaeHyeon <fholng@outlook.com>
2023-02-15 20:28:59 +0900

    10816

--- baekjoon/set/1764.py
+++ baekjoon/set/1764.py
@@ -1,18 +1,11 @@
 import sys
 
-듣, 보 = map(int, sys.stdin.readline().rstrip().split())
-듣보_목록 = {}
-결과_목록 = []
-for i in range(듣):
-    value = sys.stdin.readline().rstrip()
-    듣보_목록[value] = value
-
-for i in range(보):
-    try:
-        듣보 = 듣보_목록.pop(sys.stdin.readline().rstrip())
-        결과_목록.append(듣보)
-    except:
-        continue
-print(len(결과_목록))
-for item in sorted(결과_목록):
-    print(item)
+n, m = map(int, input().split())
+nameList = sys.stdin.read().splitlines()
+hearset = set(nameList[:n])
+seeset = set(nameList[n:])
+ret = list(hearset & seeset)
+ret.sort()
+print(len(ret))
+for i in ret:
+    print(i)
