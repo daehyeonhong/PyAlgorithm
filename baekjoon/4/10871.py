6b3623824e6027b98538cd01987fff1b7ad139a0
HongDaeHyeon <fholng@outlook.com>
2023-02-22 23:19:40 +0900

    150369.py

--- baekjoon/4/10871.py
+++ baekjoon/4/10871.py
@@ -1,20 +1,4 @@
-def solution(cap, n, deliveries, pickups):
-    answer = 0
-    while True:
-        length = len(deliveries)
-        A = 0
-        B = 0
-        while length != 0 and (cap >= A and cap >= B) and (pickups or deliveries):
-            del_pop = deliveries.pop()
-            pick_pop = pickups.pop()
-            A += del_pop
-            B += pick_pop
-        if A > cap or B > cap:
-            deliveries.append(max(A - cap, 0))
-            pickups.append(max(B - cap, 0))
-        answer += length * 2
-        if length <= 0:
-            return answer
+import sys
 
-
-print(solution(2, 2, [0, 0], [0, 4]))
+x, y = map(int, sys.stdin.readline().split())
+print(' '.join(list(filter(lambda l: int(l) < y, list(sys.stdin.readline().split())))))
--- programmers/150369.py
+++ programmers/150369.py
@@ -16,16 +16,40 @@ def solution(cap, n, deliveries, pickups):
        if devliverCount < 1 or pickupCount < 1:
             answer += lengtN
             lengtN = len(deliveries)
-            
+
        if  devliverCount < 0:
            deliveries.append(lastDeliver)
            lengtN += 1
-        
+
        if  pickupCount < 0:
            pickups.append(lastPickup)
            lengtN += 1
-  
+
     return answer
 
 
 print(solution(4, 5, [1, 0, 3, 1, 2], [0, 3, 0, 4, 0]))
+
+
+def solution(cap, n, deliveries, pickups):
+    answer = 0
+    while True:
+        A = 0
+        B = 0
+        length = 0
+        isFirstNotZero = True
+        while pickups and (cap >= A and cap >= B):
+            A += deliveries.pop()
+            B += pickups.pop()
+            if isFirstNotZero and (A != 0 or B != 0):
+                isFirstNotZero = False
+                length = len(deliveries) + 1
+        if A > cap or B > cap:
+            deliveries.append(max(A - cap, 0))
+            pickups.append(max(B - cap, 0))
+        answer += length * 2
+        if length <= 0:
+            return answer
+
+
+print(solution(2, 2, [0, 0], [0, 4]))
